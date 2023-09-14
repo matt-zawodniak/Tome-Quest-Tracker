2936dafa05bb0bfd6444ac76953f377a0936162e
matt-zawodniak <117237030+matt-zawodniak@users.noreply.github.com>
2023-09-14 16:37:10 -0400

    Fresh Start

---
+++ Quest Tracker/Quest_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quest_TrackerApp.swift
+//  Quest Tracker
+//
+//  Created by Matt Zawodniak on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Quest_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
