cb76eec26220fa2da9fc32b419cd28e7b5a1561c
matt-zawodniak <117237030+matt-zawodniak@users.noreply.github.com>
2023-12-11 14:49:25 -0500

    separated rewards
    Separated rewards into availableRewards and earnableRewards
    created Milestone vars to calculate next major reward

--- Quest Tracker/CoreDataObjects/CoreDataController.swift
+++ Quest Tracker/CoreDataObjects/CoreDataController.swift
@@ -15,13 +15,25 @@ class CoreDataController: ObservableObject {
   static var preview: CoreDataController = {
     let result = CoreDataController(inMemory: true)
     let viewContext = result.container.viewContext
-    for number in 0..<5 {
+    for number in 1..<3 {
       let newReward = Reward(context: viewContext)
       newReward.name = "Reward \(number)"
       newReward.isEarned = true
       newReward.isClaimed = false
       newReward.sortId = Int64(number)
     }
+    for number in 4..<9 {
+      let newReward = Reward(context: viewContext)
+      newReward.name = "Reward \(number)"
+      newReward.isEarned = false
+      newReward.isClaimed = false
+      newReward.sortId = Int64(number)
+    }
+    let previewUser = User(context: viewContext)
+    previewUser.currentExp = 27
+    previewUser.expToLevel = 40
+    previewUser.level = 3
+
     do {
       try viewContext.save()
     } catch {
--- RewardsView.swift
+++ RewardsView.swift
@@ -6,6 +6,7 @@
 //
 
 import CoreData
+import Foundation
 import SwiftUI
 
 struct RewardsView: View {
@@ -13,42 +14,58 @@ struct RewardsView: View {
   @FetchRequest(sortDescriptors: [],
                 predicate: NSPredicate(
                   format: "questBonusReward != nil")) var questsWithBonusRewards: FetchedResults<Quest>
-  @FetchRequest(sortDescriptors: [SortDescriptor(\.sortId)]) var rewards: FetchedResults<Reward>
-
+  @FetchRequest(sortDescriptors: [SortDescriptor(\.sortId)],
+                predicate: NSPredicate(
+                  format: "isEarned == true && isClaimed == false")) var availableRewards: FetchedResults<Reward>
+  @FetchRequest(sortDescriptors: [SortDescriptor(\.sortId)],
+                predicate: NSPredicate(format: "isEarned == false")) var earnableRewards: FetchedResults<Reward>
+  @FetchRequest(sortDescriptors: []) var userFetchResults: FetchedResults<User>
+  var user: User {
+    return userFetchResults.first!
+  }
+  var nextMilestoneLevel: Int64 {
+    var milestoneCandidate: Int64 = 0
+    for number in 0..<5 where (user.level + Int64(number)) % 5 == 0 {
+      milestoneCandidate = (user.level + Int64(number))
+    }
+    return milestoneCandidate
+  }
+  var nextMilestoneReward: String {
+    let milestoneReward = earnableRewards.first(where: {$0.sortId == nextMilestoneLevel})!
+    return milestoneReward.name ?? ""
+  }
   var body: some View {
     List {
       Section(header: Text("Unclaimed Rewards")) {
-        if rewards.isEmpty {
+        if availableRewards.isEmpty {
           Text("You have no unclaimed rewards. Keep leveling to earn more!")
         } else {
-          ForEach(rewards, id: \.self) { reward in
-            if reward.isEarned == true && reward.isClaimed == false {
-              HStack {
-                Text(reward.name!)
-                Spacer()
-                Button("Claim Reward!") {
-                  reward.isClaimed = true
-                }
+          ForEach(availableRewards, id: \.self) { reward in
+            HStack {
+              Text(reward.name!)
+              Spacer()
+              Button("Claim Reward!") {
+                reward.isClaimed = true
               }
             }
           }
         }
       }
       Section(header: Text("Next Level")) {
-        Text("Reach the next level to earn \(rewards.first?.name ?? "")!")
+        Text("Reach the next level to earn \(earnableRewards.first!.name ?? "")!")
       }
-      Section(header: Text("Next Milestone: Level 5")) {
-        Text("Keep up the good work! Reach level 5 and earn yourself a Day at the Beach!")
-      }
-    }
-      Button("Manage Rewards") {
 
+      Section(header: Text("Next Milestone: Level \(nextMilestoneLevel)")) {
+        Text("Keep up the good work! Reach level \(nextMilestoneLevel) and earn yourself \(nextMilestoneReward).")
       }
-      LevelAndExpUI()
-        .padding(.horizontal)
     }
+    Button("Manage Rewards") {
+    }
+    LevelAndExpUI()
+      .padding(.horizontal)
   }
+}
 
 #Preview {
-    RewardsView().environment(\.managedObjectContext, CoreDataController.preview.container.viewContext)
+  RewardsView().environment(\.managedObjectContext, CoreDataController.preview.container.viewContext)
 }
