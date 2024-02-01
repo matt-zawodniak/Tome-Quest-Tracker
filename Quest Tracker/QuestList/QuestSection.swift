c6baba9b97295171ca7f14e1564f00264b7488f7
matt-zawodniak <117237030+matt-zawodniak@users.noreply.github.com>
2024-02-01 16:40:20 -0500

    Implemented custom sectioned list
    from skipweeklyordaily branch

---
+++ Quest Tracker/QuestList/QuestSection.swift
@@ -0,0 +1,69 @@
+//
+//  SortedQuestList.swift
+//  Quest Tracker
+//
+//  Created by Matt Zawodniak on 2/1/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct QuestSection: View {
+
+    @Environment(\.modelContext) var modelContext
+
+    @Query var quests: [Quest]
+
+    var settings: Settings
+    var showingCompletedQuests: Bool
+    var user: User
+    var questType: QuestType
+
+  init(settings: Settings, showingCompletedQuests: Bool, user: User, questType: QuestType) {
+    _quests = Query(filter: #Predicate { $0.isCompleted == showingCompletedQuests})
+      self.settings = settings
+      self.showingCompletedQuests = showingCompletedQuests
+      self.user = user
+    self.questType = questType
+    }
+
+  var questsOfChosenType: [Quest] {
+    quests.filter({ $0.type == questType }).sorted {$0.questName < $1.questName}
+  }
+
+      var body: some View {
+        ForEach(questsOfChosenType, id: \.self) { (quest: Quest) in
+          QuestRowView(quest: quest, settings: settings)
+          .swipeActions(edge: .trailing) { Button(role: .destructive) {
+            modelContext.delete(quest)
+          } label: {
+            Label("Delete", systemImage: "trash")
+          }
+            if !showingCompletedQuests {
+              NavigationLink(destination: QuestView(
+                quest: quest, hasDueDate: quest.dueDate.exists, settings: settings)) {
+                  Button(action: {
+                  }, label: {
+                    Text("Edit")
+                  }
+                  )
+                }
+            }
+          }
+          .swipeActions(edge: .leading) {
+            if !showingCompletedQuests {
+              Button {
+                quest.isCompleted = true
+                user.giveExp(quest: quest, settings: settings, context: modelContext)
+                quest.timeCompleted = Date.now
+              } label: {
+                Image(systemName: "checkmark")
+              }
+              .tint(.green)          }
+          }
+        }    }
+  }
+  //
+  // #Preview {
+  //    QuestList()
+  // }
