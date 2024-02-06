40e1c553611902fc9a06f0a9c27a54dd869c5307
matt-zawodniak <117237030+matt-zawodniak@users.noreply.github.com>
2024-02-06 14:52:06 -0500

    stash

--- Quest Tracker/QuestList/LevelUpNotification.swift
+++ Quest Tracker/QuestList/LevelUpNotification.swift
@@ -15,29 +15,32 @@ struct LevelUpNotification: View {
 
   var body: some View {
     NavigationStack {
-      VStack {
-        HStack {
-          Image(systemName: "party.popper")
-          Text("Congratulations, you've reached level \(user.level)!").multilineTextAlignment(.center)
-          Image(systemName: "party.popper")
-            .rotation3DEffect(
-              .degrees(180),
-              axis: (x: 0.0, y: 1.0, z: 0.0)
-            )
-        }
-        HStack {
-          Button(action: {
-            isPresented = false
-          }, label: {
-            Text("Dismiss")
-          })
-          NavigationLink(destination: RewardsView(user: user)) {
+      ZStack {
+        RoundedRectangle(cornerRadius: 20)
+        VStack {
+          HStack {
+            Image(systemName: "party.popper")
+            Text("Congratulations, you've reached level \(user.level)!").multilineTextAlignment(.center)
+            Image(systemName: "party.popper")
+              .rotation3DEffect(
+                .degrees(180),
+                axis: (x: 0.0, y: 1.0, z: 0.0)
+              )
+          }
+          HStack {
             Button(action: {
-              navigateToRewardsView = true
               isPresented = false
             }, label: {
-              Text("View Rewards")
-            }).buttonStyle(.borderedProminent)
+              Text("Dismiss")
+            })
+            NavigationLink(destination: RewardsView(user: user)) {
+              Button(action: {
+                navigateToRewardsView = true
+                isPresented = false
+              }, label: {
+                Text("View Rewards")
+              }).buttonStyle(.borderedProminent)
+            }
           }
         }
       }
