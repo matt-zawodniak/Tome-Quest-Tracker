6aa516752936590ca6bb4da3e53efee30009ff1f
matt-zawodniak <117237030+matt-zawodniak@users.noreply.github.com>
2024-03-14 16:06:21 -0400

    project slug fixed, runtime error fixed

--- Quest Tracker/Quest_TrackerApp.swift
+++ Quest Tracker/Quest_TrackerApp.swift
@@ -17,6 +17,7 @@ struct Quest_TrackerApp: App {
   var container: ModelContainer = ModelController.shared.modelContainer
 
   init() {
+
     SentrySDK.start { options in
 
       options.dsn = "https://eb6119b7fd055ed8924ce9146e1c2556@o4506910806769664.ingest.us.sentry.io/4506910808342528"
