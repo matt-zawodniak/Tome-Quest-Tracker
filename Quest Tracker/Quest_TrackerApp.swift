43c84304401fc17f3bc3bbb89c595c11a2d6ab16
matt-zawodniak <117237030+matt-zawodniak@users.noreply.github.com>
2024-03-22 15:01:17 -0400

    whitespace

--- Quest Tracker/Quest_TrackerApp.swift
+++ Quest Tracker/Quest_TrackerApp.swift
@@ -17,15 +17,11 @@ struct Quest_TrackerApp: App {
   var container: ModelContainer = ModelController.shared.modelContainer
 
   init() {
-
     SentrySDK.start { options in
-
       options.dsn = "https://eb6119b7fd055ed8924ce9146e1c2556@o4506910806769664.ingest.us.sentry.io/4506910808342528"
-
     }
 
     QuestTrackerShortcuts.updateAppShortcutParameters()
-
   }
 
   var body: some Scene {
