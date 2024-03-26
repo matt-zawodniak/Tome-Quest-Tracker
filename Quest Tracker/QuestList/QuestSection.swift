9dcfc5716ae21eb24494527117297c4206c4ed07
matt-zawodniak <117237030+matt-zawodniak@users.noreply.github.com>
2024-03-26 18:45:20 -0400

    deleted edit from swipe actions

--- Quest Tracker/QuestList/QuestSection.swift
+++ Quest Tracker/QuestList/QuestSection.swift
@@ -44,16 +44,6 @@ struct QuestSection: View {
           } label: {
             Label("Delete", systemImage: "trash")
           }
-          if !showingCompletedQuests {
-            NavigationLink(destination: QuestView(
-              quest: quest, hasDueDate: quest.dueDate.exists, settings: settings)) {
-                Button(action: {
-                }, label: {
-                  Text("Edit")
-                }
-                )
-              }
-          }
         }
         .swipeActions(edge: .leading) {
           if !showingCompletedQuests {
