9a4fe47ee6bff97a8c289777fb9f87e80f5950ea
matt-zawodniak <117237030+matt-zawodniak@users.noreply.github.com>
2024-05-17 16:59:23 -0400

    removed QuestTrackerModel

--- Quest Tracker/QuestTrackerViewModel.swift
+++ Quest Tracker/QuestTrackerViewModel.swift
@@ -9,15 +9,6 @@ import SwiftUI
 import SwiftData
 
 class QuestTrackerViewModel: ObservableObject {
-
-  @Published var trackerModel = QuestTrackerModel()
-
-  func deselectQuests(quests: [Quest], context: ModelContext) {
-    for quest in quests {
-      quest.isSelected = false
-    }
-  }
-
   func sortDescriptorFromSortType(sortType: QuestSortDescriptor) -> SortDescriptor<Quest> {
     switch sortType {
     case .dueDate: SortDescriptor(\Quest.dueDate, order: .reverse)
@@ -54,14 +45,6 @@ enum QuestSortDescriptor: Int64, CaseIterable, CustomStringConvertible {
 }
 
 extension Date {
-  var string: String {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateStyle = .short
-    dateFormatter.timeStyle = .short
-
-    return dateFormatter.string(from: self)
-  }
-
   var dayOnly: String {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "EEEE"
@@ -101,18 +84,6 @@ extension Optional where Wrapped == Date {
       return false
     } else {
       return true
-    }
-  }
-
-  var string: String {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateStyle = .short
-    dateFormatter.timeStyle = .short
-
-    if self == nil {
-      return ""
-    } else {
-      return dateFormatter.string(from: self!)
     }
   }
 
