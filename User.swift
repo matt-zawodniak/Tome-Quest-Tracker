cd1d10e99a3e2f741905f77cb3a98b0f8143ae28
matt-zawodniak <117237030+matt-zawodniak@users.noreply.github.com>
2024-05-21 18:16:09 -0400

    Level and exp UI refactor (#64)
    * centered exp bar
    
    * Bad dispatchQueue attempt on LevelUp instead of animation
    
    * Adjusted frame to stop resizing
    
    * reverted botched dispatchqueue attempt
    
    * animates but incorrectly
    
    * PhaseAnimator + DispatchQueue Attempt
    
    * Animation working
    
    Going to full might be a little weird.
    
    * fixed animation bug
    
    Fixed animation with leveling scheme 1
    removed testing buttons
    
    * decoupled leveling animation from exp
    
    * decoupled exp bar from strict values during animation

--- User.swift
+++ User.swift
@@ -15,8 +15,9 @@ import SwiftData
   var expToLevel: Double = 0.0
   var level: Int64 = 0
   var levelingScheme: Int64 = 0
+  var isLevelingUp: Bool = false
 
-  public init(currentExp: Double, expToLevel: Double, level: Int64, levelingScheme: Int64) {
+  public init(currentExp: Double, expToLevel: Double, level: Int64, levelingScheme: Int64, isLevelingUp: Bool) {
     self.currentExp = currentExp
     self.expToLevel = expToLevel
     self.level = level
@@ -28,10 +29,10 @@ extension User: Identifiable {
   func giveExp(quest: Quest, settings: Settings, context: ModelContext) {
     let questExp = quest.type.experience * quest.questDifficulty.expMultiplier * quest.questLength.expMultiplier
 
-    currentExp += questExp
+    if currentExp + questExp >= expToLevel {
+      currentExp += questExp
 
-    if currentExp >= expToLevel {
-      levelUp(settings: settings)
+      levelUp()
 
       if level % 5 == 0 {
         var milestoneRewardFetchedResults: [Reward]? {
@@ -73,6 +74,8 @@ extension User: Identifiable {
           firstMinorReward.dateEarned = Date()
         }
       }
+    } else {
+      currentExp += questExp
     }
   }
 
@@ -90,7 +93,7 @@ extension User: Identifiable {
       context.insert(unearnedCopyofReward)
     }
 
-  static var defaultUser: User = User(currentExp: 0, expToLevel: 60, level: 1, levelingScheme: 0)
+  static var defaultUser: User = User(currentExp: 0, expToLevel: 60, level: 1, levelingScheme: 0, isLevelingUp: false)
 
   static func fetchFirstOrCreate(context: ModelContext) -> User {
     let userRequest = FetchDescriptor<User>()
@@ -102,10 +105,10 @@ extension User: Identifiable {
     return user
   }
 
- func levelUp(settings: Settings) {
-    level += 1
+  func levelUp() {
+     level += 1
 
-    currentExp -= expToLevel
+     currentExp -= expToLevel
 
     if levelingScheme == 1 {
       expToLevel += 20
