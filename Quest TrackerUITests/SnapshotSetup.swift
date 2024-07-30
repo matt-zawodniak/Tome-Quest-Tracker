5b39567aa13fce7b2cc16ae9523d0624bab20a31
matt-zawodniak <117237030+matt-zawodniak@users.noreply.github.com>
2024-07-30 01:55:52 -0400

    One test with first user interaction cycle
    1. Opens Rewards, navigates through sheets to add both a minor and a major reward before dismissing sheet.
    2. Adds a new quest.

--- Quest TrackerUITests/SnapshotSetup.swift
+++ Quest TrackerUITests/SnapshotSetup.swift
@@ -19,111 +19,59 @@ final class SnapshotSetup: XCTestCase {
     let app = XCUIApplication()
     app.launch()
 
-//    let giftImage = app.images["Gift"]
-//    giftImage.tap()
-//
     let collectionViewsQuery = app.collectionViews
-//    collectionViewsQuery.staticTexts["Manage Rewards"].tap()
-//
-//    let addRewardStaticText = collectionViewsQuery.staticTexts["Add Reward"]
-//    addRewardStaticText.tap()
-//
-//    let rewardNameTextField = collectionViewsQuery.textFields["Reward Name"]
-//    rewardNameTextField.tap()
-//    rewardNameTextField.typeText("Ice Cream")
-//
-//    let backButton = app.navigationBars["_TtGC7SwiftUI32NavigationStackHosting"].buttons["Back"]
-//    backButton.tap()
-//    addRewardStaticText.tap()
-//    rewardNameTextField.tap()
-//    rewardNameTextField.typeText("Dinner Out")
-//    collectionViewsQuery.buttons["Milestone"].tap()
-//    backButton.tap()
-//    backButton.tap()
-//
-//    let nextLevelStaticText = collectionViewsQuery.staticTexts["Next Level"]
-//    let start = nextLevelStaticText.coordinate(withNormalizedOffset: CGVector(dx: 0.0, dy: 0.0))
-//    let finish = nextLevelStaticText.coordinate(withNormalizedOffset: CGVector(dx: 0.0, dy: 50.0))
-//    start.press(forDuration: 0.5, thenDragTo: finish)
-//
-//    app.images["plus.square.dashed"].tap()
-//
+
+    let giftImage = app.images["Gift"]
+    giftImage.tap()
+    let manageRewardsText = collectionViewsQuery.staticTexts["Manage Rewards"]
+    XCTAssert(manageRewardsText.waitForExistence(timeout: 30))
+
+    collectionViewsQuery.staticTexts["Manage Rewards"].tap()
+
+    let addRewardStaticText = collectionViewsQuery.staticTexts["Add Reward"]
+    XCTAssert(addRewardStaticText.waitForExistence(timeout: 30))
+
+    addRewardStaticText.tap()
+
+    let rewardNameTextField = collectionViewsQuery.textFields["Reward Name"]
+    XCTAssert(rewardNameTextField.waitForExistence(timeout: 30))
+
+    rewardNameTextField.tap()
+    rewardNameTextField.typeText("Ice Cream")
+
+    let backButton = app.navigationBars["_TtGC7SwiftUI32NavigationStackHosting"].buttons["Back"]
+    backButton.tap()
+    XCTAssert(collectionViewsQuery.staticTexts["Ice Cream"].waitForExistence(timeout: 30))
+
+    addRewardStaticText.tap()
+    rewardNameTextField.tap()
+    rewardNameTextField.typeText("Dinner Out")
+    collectionViewsQuery.buttons["Milestone"].tap()
+    backButton.tap()
+    XCTAssert(collectionViewsQuery.staticTexts["Dinner Out"].waitForExistence(timeout: 30))
+
+    backButton.tap()
+
+    let nextLevelStaticText = collectionViewsQuery.staticTexts["Next Level"]
+    let start = nextLevelStaticText.coordinate(withNormalizedOffset: CGVector(dx: 0.0, dy: 0.0))
+    let finish = nextLevelStaticText.coordinate(withNormalizedOffset: CGVector(dx: 0.0, dy: 50.0))
+    start.press(forDuration: 0.5, thenDragTo: finish)
+    XCTAssert(manageRewardsText.exists == false)
+
+    app.images["plus.square.dashed"].tap()
+
     let questNameTextField = collectionViewsQuery.textFields["Quest Name"]
-//    questNameTextField.tap()
-//    questNameTextField.typeText("Download Tome")
-//    app.buttons["Create"].tap()
+    XCTAssert(questNameTextField.waitForExistence(timeout: 30))
 
-    let plusImage = app.images["plus.square.dashed"]
-    plusImage.tap()
-
-    collectionViewsQuery.staticTexts["Main Quest"].tap()
-    let dailyText = app.staticTexts["Daily Quest"]
-    XCTAssert(dailyText.waitForExistence(timeout: 30))
-    collectionViewsQuery.staticTexts["Daily Quest"].tap()
     questNameTextField.tap()
-    questNameTextField.typeText("Exercise")
+    questNameTextField.typeText("Download Tome")
     app.buttons["Create"].tap()
+    XCTAssert(collectionViewsQuery.staticTexts["Create"].exists == false)
+    XCTAssert(collectionViewsQuery.staticTexts["Download Tome"].waitForExistence(timeout: 30))
 
-//    app.images["plus.square.dashed"].tap()
-//
-//    questNameTextField.tap()
-//    questNameTextField.typeText("Walk the Dog")
-//    app.pickerWheels.firstMatch.adjust(toPickerWheelValue: "Daily Quest")
-//    app.buttons["Create"].tap()
-//
-//    app.images["plus.square.dashed"].tap()
-//
-//    questNameTextField.tap()
-//    questNameTextField.typeText("Food Shopping")
-//    app.pickerWheels.firstMatch.adjust(toPickerWheelValue: "Weekly Quest")
-//    app.buttons["Create"].tap()
-//
-//    app.images["plus.square.dashed"].tap()
-//
-//    questNameTextField.tap()
-//    questNameTextField.typeText("Take Out Trash")
-//    app.pickerWheels.firstMatch.adjust(toPickerWheelValue: "Weekly Quest")
-//    app.buttons["Create"].tap()
-//
-//    app.images["plus.square.dashed"].tap()
-//
-//    questNameTextField.tap()
-//    questNameTextField.typeText("Clean Garage")
-//    app.pickerWheels.firstMatch.adjust(toPickerWheelValue: "Side Quest")
-//    app.buttons["Create"].tap()
-//
-//    app.images["plus.square.dashed"].tap()
-//
-//    questNameTextField.tap()
-//    questNameTextField.typeText("Laundry")
-//    app.pickerWheels.firstMatch.adjust(toPickerWheelValue: "Weekly Quest")
-//    app.buttons["Create"].tap()
-//
-//    app.images["plus.square.dashed"].tap()
-//
-//    questNameTextField.tap()
-//    questNameTextField.typeText("Make Car Service Appt")
-//    app.buttons["Create"].tap()
-//
-//    collectionViewsQuery.staticTexts["Download Tome"].swipeRight()
-//
-//    let selectedButton = collectionViewsQuery.buttons["Selected"]
-//    selectedButton.tap()
-//
-//    let exerciseStaticText = collectionViewsQuery.staticTexts["Exercise"]
-//    exerciseStaticText.swipeLeft()
-//
-//    collectionViewsQuery.staticTexts["Daily Quests"].tap()
-//
-//    let foodShoppingStaticText = collectionViewsQuery.staticTexts["Food Shopping"]
-//    foodShoppingStaticText.swipeRight()
-//    selectedButton.tap()
-//
-//    giftImage.tap()
-//    collectionViewsQuery.buttons["Claim Reward!"].tap()
-//    start.press(forDuration: 0.5, thenDragTo: finish)
-//
-//    app.images["gearshape"].tap()
+    let questName = collectionViewsQuery.staticTexts["Download Tome"]
+    questName.tap()
+    XCTAssert(app.buttons["Confirm"].waitForExistence(timeout: 30))
   }
 
   override func tearDownWithError() throws {
