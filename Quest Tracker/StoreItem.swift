e4b42863b3fbc5e1b22ce1ef1874d728f984d8b2
matt-zawodniak <117237030+matt-zawodniak@users.noreply.github.com>
2024-08-07 15:06:36 -0400

    In-App Purchase (#75)
    * Implemented purchasing
    
    * isPurchased refactor
    
    * Fixed indentation

---
+++ Quest Tracker/StoreItem.swift
@@ -0,0 +1,48 @@
+//
+//  StoreItem.swift
+//  Quest Tracker
+//
+//  Created by Matt Zawodniak on 8/7/24.
+//
+
+import SwiftUI
+import StoreKit
+
+struct StoreItem: View {
+  @ObservedObject var storeKit: StoreKitManager
+  @State var isPurchased: Bool = false
+  var product: Product
+
+  var body: some View {
+    if isPurchased {
+
+      Button(action: {},
+             label: {
+        HStack {
+          Spacer()
+          Text("Thank you for supporting us!")
+            .foregroundStyle(.cyan)
+          Spacer()
+        }
+      })
+    } else {
+      Button(action: {
+        Task {
+          try await storeKit.purchase(product: product)
+        }
+      }, label: {
+        HStack {
+          Text(product.displayName)
+          Spacer()
+          Text(product.displayPrice)
+        }
+        .foregroundStyle(.white)
+        .onChange(of: storeKit.purchasedProducts) {
+          Task {
+            isPurchased = (try? await storeKit.customerHasPurchased(product: product)) ?? false
+          }
+        }
+      })
+    }
+  }
+}
