441116155c00ff1e9a4619254897f3a1ac770fe5
matt-zawodniak <117237030+matt-zawodniak@users.noreply.github.com>
2024-08-28 14:11:36 -0400

    UI test (#73)
    * Section Header fix
    
    Section now only shows count of active or completed quests when collapsed, depending on view mode
    
    * deleted edit from swipe actions
    
    * Update QuestSection.swift
    
    added restore to active quests swipe action.
    included potential ToDos
    removed extra vars and .sheet never called
    
    * Update QuestSection.swift
    
    * added skip to swipe actions
    
    * Deleted obsolete code from QuestRowView
    
    * Obsoleting QuestDetailView
    
    * refactor
    
    * Remove QuestDetailView
    
    * lint
    
    * changed naming of vars
    
    * Comment fixes
    
    * Started QuestView
    
    Started adding buttons to QuestView.
    Need to decide which buttons should be visible in each state (editing, completed vs active, etc)
    
    * Added buttonview to QuestView
    
    * Update QuestView.swift
    
    * Buttons moved to own section
    
    * Fixed dismissing but needs environment objects
    
    * Used Env vars for QuestView popup
    
    * Button fix
    
    * Cleaned up button code
    
    * removed comment
    
    * fixed merge oversight
    
    * merge update
    
    * Removed EditingQuestHandler
    
    * Changes from comments
    
    * complete, skip, restore made into func
    
    * Update ButtonSection.swift
    
    * update
    
    * exp bar size bugfix
    
    * UITest fails to tap dropdown menu
    
    * Update Fastfile
    
    * stopped build testing
    
    * Update Fastfile
    
    * One test with first user interaction cycle
    
    1. Opens Rewards, navigates through sheets to add both a minor and a major reward before dismissing sheet.
    2. Adds a new quest.
    
    * reinitiated pr checks
    
    * Simplified test, split into two tests
    
    * Lint

---
+++ Quest TrackerUITests/Quest_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Quest_TrackerUITestsLaunchTests.swift
+//  Quest TrackerUITests
+//
+//  Created by Matt Zawodniak on 7/19/24.
+//
+
+import XCTest
+
+final class Quest_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
